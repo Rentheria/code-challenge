b5304359996834693b94e7b3a3c3c8762e2b3475
Rentheria <manuel.rentheria@gmail.com>
2024-02-29 14:31:21 -0600

    solve home.js

--- ticTacToe/src/home/home.js
+++ ticTacToe/src/home/home.js
@@ -1,21 +1,18 @@
-const MARK_X = 'X';
-const MARK_O = 'O';
-
-export function startGame() {
+function startGame() {
     window.location.href = "../game/game.html";
 }
 
-export function storeOption(input) {
+function storeOption(input) {
     localStorage.setItem("mark", input);
 } 
 
-export function getPlayerChoice() {
+function getPlayerChoice() {
     const choice = prompt("Player 1, please select 'X' or 'O' to start:");
     if (choice === null) {
         return null;
     }
     const player1Mark = choice.toUpperCase();
-    if (player1Mark === MARK_X || player1Mark === MARK_O) {
+    if (player1Mark === 'X' || player1Mark === 'O') {
         return player1Mark;
     } else {
         alert("Invalid choice! Please select 'X' or 'O'.");
@@ -23,7 +20,7 @@ export function getPlayerChoice() {
     }
 }
 
-export function initializeGame() {
+function initializeGame() {
     const player1Choice = getPlayerChoice();
     if (player1Choice !== null) {
         storeOption(player1Choice);
